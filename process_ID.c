7c6bccc1e558f6006e02e503432466201b1b3b5d
Boodenator <boode1000@gmail.com>
2025-05-18 18:04:44 +0300

    process ID

---
+++ process_ID.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+        /*ppid value*/
+        printf("pid equals = %d\n", getppid());
+
+        /*MAX value ppid*/
+        printf
+        return 0;
+}
