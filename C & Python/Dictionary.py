b373f3c539e398c01899116d1a1758fca3b15976
Monica-Holberton <monicafarid7@gmail.com>
2025-07-01 08:56:46 +0000

    Add and Change Value!

---
+++ C & Python/Dictionary.py
@@ -0,0 +1,13 @@
+
+fruits = {
+    "apple": 1,
+    "banana": 2,
+    "cherry": 3
+}
+print("Original dictionary:", fruits)
+
+fruits["mango"] = 3
+print("After adding 'mango':", fruits)
+
+fruits["banana"] = 5
+print("After changing 'banana' quantity:", fruits)
